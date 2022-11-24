08a8a8cf01ed138ee9c43f1fa319c0bccb4b3997
sachinthaDilshan <sachidisanayaka1998@gmail.com>
2022-11-24 17:18:48 +0530

    begin

---
+++ src/main/java/com/assignment/grpc/GrpcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/grpc/GrpcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
